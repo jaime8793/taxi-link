0f585d2733a4b0738b35ae1a1bc81391aeab1e89
jaime <jamesowiti24@gmail.com>
2023-07-05 16:19:38 +0300

    Basic File structure improvement and Tailwind install

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Login from './Login';
+import Signup from './Signup';
+import Homepage from './Homepage';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-      
-
+    <div>
+      <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+      <Footer />
     </div>
+
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar() {
+  // Define your navigation bar here
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Signup</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    </div>
+    
+  );
+}
+
+export default Navbar;
