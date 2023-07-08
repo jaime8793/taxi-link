284c482a11d2e83be26791069dd32bb3add36e13
jaime <jamesowiti24@gmail.com>
2023-07-08 09:43:36 +0300

    Added basic info to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,200 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <footer class="bg-footer_background text-white py-4 text-center">
-  <p class="mb-2">&copy; 2023 Your Company. All rights reserved.</p>
-  <p>
-    Follow us on social media:
-    <span class="inline-flex space-x-2 mt-2">
-      <a href="#" class="text-white hover:text-gray-300"><img src="facebook-icon.png" alt="Facebook" class="w-6 h-6"/></a>
-      <a href="#" class="text-white hover:text-gray-300"><img src="twitter-icon.png" alt="Twitter" class="w-6 h-6"/></a>
-      <a href="#" class="text-white hover:text-gray-300"><img src="instagram-icon.png" alt="Instagram" class="w-6 h-6"/></a>
-    </span>
-  </p>
-</footer>
+  return (
+    <footer class="bg-footer_background text-white py-4 text-center">
+      <div class="container flex-row
+      ">
+        <div class="row ">
+          <div class="col-md-4 mb-3 mb-md-0">
+            <h6 class="text-uppercase mb-3">Customer services</h6>
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">Help &amp; Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Returns &amp; Refunds</a>
+              </li>
+              <li>
+                <a href="#">Online Stores</a>
+              </li>
+              <li>
+                <a href="#">Terms &amp; Conditions</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4 mb-3 mb-md-0">
+            <h6 class="text-uppercase mb-3">Company</h6>
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">What We Do</a>
+              </li>
+              <li>
+                <a href="#">Available Services</a>
+              </li>
+              <li>
+                <a href="#">Latest Posts</a>
+              </li>
+              <li>
+                <a href="#">FAQs</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <h6 class="text-uppercase mb-3">Social media</h6>
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">Tumblr</a>
+              </li>
+              <li>
+                <a href="#">Pinterest</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr class="my-4" />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 d-flex align-items-center justify-content-center justify-content-md-start mb-4 mb-md-0">
+            <p class="mb-0">
+              © 2023 Your company. Download From{" "}
+              <a
+                href="https://themes.bootstrapious.com/"
+                class="text-white"
+                target="_blank"
+              >
+                Themes Bootstrapious
+              </a>
+              .
+            </p>
+          </div>
+          <div class="col-md-6 d-flex align-items-center justify-content-center justify-content-md-end">
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <img
+                  src="https://themes.bootstrapious.com/sell/assets/img/visa.svg"
+                  alt="..."
+                  class="w-2rem"
+                />
+              </li>
+              <li class="list-inline-item">
+                <img
 
-    );
-    }
+                  src="https://themes.bootstrapious.com/sell/assets/img/mastercard.svg"
+                  alt="..."
+                  class="w-2rem"
+                />
+              </li>
+              <li class="list-inline-item">
+                <img
+                  src="https://themes.bootstrapious.com/sell/assets/img/paypal.svg"
+                  alt="..."
+                  class="w-2rem"
+                />
+              </li>
+              <li class="list-inline-item">
+                <img
+                  src="https://themes.bootstrapious.com/sell/assets/img/western-union.svg"
+                  alt="..."
+                  class="w-2rem"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr class="my-4" />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 mb-3 mb-md-0">
+            <h6 class="text-uppercase">Top categories</h6>
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">Furniture</a>
+              </li>
+              <li>
+                <a href="#">Dresses</a>
+              </li>
+              <li>
+                <a href="#">T-shirts</a>
+
+              </li>
+              <li>
+                <a href="#">Bags</a>
+              </li>
+              <li>
+                <a href="#">Shoes</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3 mb-3 mb-md-0">
+
+            <h6 class="text-uppercase">Main features</h6>
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">Super Fast</a>
+              </li>
+              <li>
+                <a href="#">Free Delivery</a>
+              </li>
+              <li>
+                <a href="#">Free Replacement</a>
+              </li>
+              <li>
+                <a href="#">24/7 Support</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3 mb-3 mb-md-0">
+
+            <h6 class="text-uppercase">Pages</h6>
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Terms and conditions</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3 mb-3 mb-md-0">
+
+
+            <h6 class="text-uppercase">Social media</h6>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">Tumblr</a>
+              </li>
+              <li>
+                <a href="#">Pinterest</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <p class="mb-2">&copy; 2023 Your Company. All rights reserved.</p>
+    </footer>
+  );
+}
 
 export default Footer;
